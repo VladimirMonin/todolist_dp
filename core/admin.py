0d9633bac628f37ddaee7fe9416337cb3f9c65f4
Vladimir Monin <moninvladimir88@gmail.com>
2023-01-09 14:29:07 +0600

    hw-33

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from core.models import User
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    exclude = ('password',)
+    readonly_fields = ('last_login', 'date_joined')
