8d92b8396e1362d53bcaefe89d02970c1b0a96f7
Vladimir Monin <moninvladimir88@gmail.com>
2023-01-26 11:09:10 +0600

    lesson-38: add bot's code

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import User
 
-# Create your models here.
+
+class TgUser(models.Model):
+    class Meta:
+        verbose_name = 'Пользователь Telegram'
+        verbose_name_plural = 'Пользователи Telegram'
+
+    tg_id = models.IntegerField(verbose_name='ID чата')
+    username = models.CharField(max_length=50, verbose_name='Username пользователя в tg', null=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.PROTECT, null=True)
+    verification_code = models.CharField(max_length=150, verbose_name='Код верификации')
